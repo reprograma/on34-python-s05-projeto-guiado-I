33e1491d7af5db0dedffac8c16f528f1450cdda9
Williane Fernandes <williane.sena.fernandes@gmail.com>
2024-06-29 12:05:23 -0300

    Adicionando a segunda versão do projeto

--- projeto_guiado_williane.py
+++ projeto_guiado_williane.py
@@ -1,28 +1,54 @@
-def media_notas ():
-    while True:
-        try:
-            print("**Calculadora para média de notas** \nPara realizar o cálculo, digite o nome da aluna e as três notas que deseja calcular a média. \n Não são permitidos: \n⇨ Valores que não sejam números para as notas\n⇨ Inserir número negativo")
-            nome = input("Nome da aluna: ")
-            nota1 = float(input("Insira a primeira nota: "))
-            nota2 = float(input("Insira a segunda nota: "))
-            nota3 = float(input("Insira a terceira nota: "))
+dataset = {}
 
-            media = (nota1 + nota2 + nota3)/3
+print("-- Calculadora para média de notas -- \nPara realizar o cálculo, digite os dados da aluna e as três notas que deseja calcular a média. \n Não são permitidos: \n⇨ Valores que não sejam números para as notas\n⇨ Inserir número negativo")
 
-            if media < 6:
-                print("A aluna", nome, "foi reprovada com média igual a:", round(media, 2))
-            
-            else:
-                print("A aluna", nome, "foi aprovada com média igual a:", round(media, 2))
+def dados_aluna ():
+    global nome
+    nome = input("Insira o nome da aluna: ")
+    turma = input("Insira a turma da aluna: ")
 
-            continuar_programa = input("Digite 1 se deseja calcular uma nova média ou 2 caso deseje parar: ")
-
-            if continuar_programa == '1':
-                continue
-            else:
+    quantidade_notas = input("Insira a quantidade de notas: ")
+    notas_aluna = []
+ 
+    for contador in range(int(quantidade_notas)):
+        while True:
+            entrada = input(f"Insira a nota #{contador + 1}: ")
+            try:
+                nota = float(entrada)
+                notas_aluna.append(nota)
                 break
-                
-        except:
-            print("Entrada inválida.")
+            except ValueError:
+                print("Entrada inválida")
+    
+    print(f"Notas inseridas: {notas_aluna}")
 
-media_notas()
+    media_notas(nome, turma, notas_aluna)
+    
+def media_notas(nome, turma, notas_aluna):
+    media = sum(notas_aluna)/ len(notas_aluna)
+            
+    if media < 6:
+        print("A aluna", nome, "foi reprovada com média igual a:", round(media, 2))
+            
+    else:
+        print("A aluna", nome, "foi aprovada com média igual a:", round(media, 2))
+
+    continuar_programa = input("Digite 1 se deseja calcular uma nova média ou 2 caso deseje parar: ")
+
+    if continuar_programa == '1':
+        dados_aluna()
+    else:
+        print("Programa encerrado")
+    
+    salvar_dados_aluna(nome, turma, notas_aluna, media)   
+
+def salvar_dados_aluna(nome, turma, notas_aluna, media):
+    chave = (nome)
+    dataset[chave] = {
+        "Turma" : turma,
+        "Notas aluna": notas_aluna,
+        "Media aluna": media       
+        }
+    print(f"Dados aluna {chave}: {dataset[chave]}")
+
+dados_aluna()
