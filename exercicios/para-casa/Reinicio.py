580a9c2b4be2ee65f495b433cdd033190ce21479
Polliferraz <polliferraz@yahoo.com.br>
2024-07-03 09:49:30 -0300

    Reiniciei mais uma vez

---
+++ exercicios/para-casa/Reinicio.py
@@ -0,0 +1,77 @@
+from dataset_alunas import dataset
+
+def main():
+    print("\n---  Seja bem vinda a Escola do Reprograma!  ---")
+    print("Sistema de informações de alunas")
+   
+    while True:
+        cod_opcao = obter_opcao()
+        
+        if cod_opcao == 1: incluir_nova_aluna()
+        elif cod_opcao == 2: consultar_lista_alunas()
+        elif cod_opcao == 3: consultar_faltas_aluna()
+        elif cod_opcao == 4: consultar_notas_aluna()
+        elif cod_opcao == 5: consultar_status_aprovacao()
+        elif cod_opcao == 6: print("Encerrando o programa..."); break
+
+def obter_opcao():
+    codigo_opcao = 0
+
+    while codigo_opcao not in [1, 2, 3, 4, 5, 6]:
+        try:
+            codigo_opcao = int(input("\nEscolha uma opção:\n"
+                                    "1 - Incluir uma nova aluna\n"
+                                    "2 - Consultar lista de alunas\n"
+                                    "3 - Consultar faltas da aluna\n"
+                                    "4 - Consultar notas da aluna\n"
+                                    "5 - Consultar status de aprovação\n"
+                                    "6 - Sair do sistema\n"
+                                    "Opção: "))
+                
+            if codigo_opcao not in [1, 2, 3, 4, 5, 6]:
+                print("Opção inválida. Por favor, escolha uma opção válida (1 a 5).\n")
+        except ValueError:
+            print("Entrada inválida. Por favor, digite um número inteiro.\n")
+            
+        return codigo_opcao
+    
+def incluir_nova_aluna():
+    print("Insira os seguintes dados: ")
+    nome = str(input("Nome da aluna: ")) #Recebo nome da aluna
+    sobrenome = str(input("Sobrenome da aluna: ")) #Recebo sobrenome da aluna
+    turma = int(input("Turma da aluna (nº): ")) #Recebo nome a turma
+    lista_presenca = consultar_faltas_aluna() #Recebo a lista de presença com 'True' ou 'False'
+    lista_notas = consultar_notas_aluna() #Chamo a função de pegar as notas para alimentar minha lista "notas"
+    nota_participacao = float(input("Participação da aluna: ")) #Recebo nota de participação
+    print('Aluna adicionada com sucesso!' )
+       
+    return nome
+'''
+def consultar_lista_alunas():
+    nome = str(input('Digite o nome da Aluna: '))
+    sobrenome = str(input('Digite o sobrenome da Aluna: '))
+    consulta = dataset(nome, sobrenome)
+    if consulta:
+        print(f' Nome:{consulta[0]} Sobrenome: {consulta[1]} \n'
+                'Faltas: {consulta[3]}\n'
+                'Notas: {consulta[4]}\n'
+                'Situação: {consulta[5]}\n')
+    else:
+        print("Consulta não encontrada.")
+    return consulta
+
+consultar_lista_alunas()'''
+
+def consultar_faltas_aluna():
+    pass
+    #TODO - Implentar a função
+    
+def consultar_notas_aluna():
+    pass
+    #TODO - Implentar a função
+    
+def consultar_status_aprovacao():
+    pass
+    #TODO - Implentar a função
+    
+main()
