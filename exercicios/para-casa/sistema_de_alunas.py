0364160553dd62fd92754dc9c724de978b7fdf51
Maria Camila <alcantaracamila59@gmail.com>
2024-07-05 17:50:55 -0300

    Projeto Guiado - Maria Camila

--- exercicios/para-casa/sistema_de_alunas.py
+++ exercicios/para-casa/sistema_de_alunas.py
@@ -36,24 +36,129 @@ def obter_opcao():
         return codigo_opcao
     
 def incluir_nova_aluna():
-    pass
-    #TODO - Implentar a função
-    
+    print("Por favor insira estes dados: ")
+    nome = input("Nome da aluna: ")
+    sobrenome = input("Sobrenome da aluna: ")
+    turma = input("Turma da aluna: ")
+    lista_presenca = obter_presenca()
+    nota_participacao = float(input("Participação da aluna: "))
+    notas = obter_notas()
+
+    salvar_dados_aluna(nome, sobrenome, turma, lista_presenca, nota_participacao, notas)
+    print("Aluna adicionada com sucesso!")
+
+def obter_presenca():
+    quantidade_aulas = input("Quantidade de aulas: ")
+    aulas = []
+
+    for contador in range(int(quantidade_aulas)):
+        while True:
+            entrada = input(f"Insira a presença da aula #{contador + 1} (True ou False): ")
+            if entrada in ["True", "False"]:
+                presenca = entrada == "True"
+                aulas.append(presenca)
+                break
+            else:
+                print("Entrada inválida. Por favor, insira True ou False.")
+
+    return aulas
+
+def obter_notas():
+    quantidade_notas = input("Quantidade de notas: ")
+    notas = []
+
+    for contador in range(int(quantidade_notas)):
+        while True:
+            entrada = input(f"Insira a nota #{contador + 1}: ")
+            try:
+                nota = float(entrada)
+                if nota >= 0 and nota <= 10:
+                    notas.append(nota)
+                    break
+                else:
+                    print("Nota inválida. Por favor, insira uma nota entre 0 e 10.")
+                    continue
+            except ValueError:
+                print("Entrada inválida. Por favor, insira um número válido.")
+             
+
+    return notas
+
+def salvar_dados_aluna(nome, sobrenome, turma, lista_presenca, nota_participacao, notas):
+    chave = f"{nome} {sobrenome}"
+    dataset[chave] = {
+        "Nome": nome,
+        "Sobrenome": sobrenome,
+        "Turma": turma,
+        "Notas": notas,
+        "Presença": lista_presenca,
+        "Participação": nota_participacao
+    }
+
 def consultar_lista_alunas():
-    pass
-    #TODO - Implentar a função
-    
+    if not dataset:
+        print("Nenhuma aluna cadastrada.")
+        return
+
+    print("\nLista de alunas:")
+    for nome in dataset:
+        print(f"- {nome}")
+
+
+
 def consultar_faltas_aluna():
-    pass
-    #TODO - Implentar a função
+    try:
+        nome = input("Nome da aluna: ")
+        sobrenome = input("Sobrenome da aluna: ")
+        lista_presenca = dataset[(nome, sobrenome)]["Presença"]
+        if (nome, sobrenome) in dataset:
+            print(f"A aluna {nome} {sobrenome} está com {lista_presenca}")
+        
+    except:
+        print("Aluna não encontrada!")    
+
     
 def consultar_notas_aluna():
-    pass
-    #TODO - Implentar a função
+    try:
+        nome = input("Nome da aluna: ")
+        sobrenome = input("Sobrenome da aluna: ")
+        notas_aluna = dataset[(nome, sobrenome)]["Notas"]
+        if (nome, sobrenome) in dataset:
+            print(f"A aluna {nome} {sobrenome} está com essas notas {notas_aluna}")
+
+    except:
+        print("Aluna não encontrada!")        
+
+    
     
 def consultar_status_aprovacao():
-    pass
-    #TODO - Implentar a função
-    
+    while True:
+        nome = input("Nome da aluna: ")
+        sobrenome = input("Sobrenome da aluna: ")
+        
+        chave = (nome, sobrenome)
+        
+        if chave in dataset:
+            dados = dataset[chave]
+            notas = dados["Notas"]
+            soma = sum(notas)
+            participacao = dados["Participação"]
+            qtd_presenca = sum(dados["Presença"])
+            percentual_presenca = qtd_presenca / len(dados["Presença"])
+            nota_media = soma / len(notas)
+
+            print(f"A aluna {nome} {sobrenome} está com a nota média {nota_media:.2f}")
+            if nota_media >= 6 and percentual_presenca >= 0.80 and participacao >= 6:
+                print("Aluna aprovada!!")
+            else:
+                print("Aluna reprovada!!")
+        else:
+            print("Aluna não encontrada no dataset.")
+        
+        continuar = input("Deseja consultar outra aluna? (s/n): ")
+        if continuar.lower() != 's':
+            break
+        
+
 
 main()
