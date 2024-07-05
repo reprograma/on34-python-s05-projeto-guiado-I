9993d890e80ec66871429ba42f842702904ab25d
TmTeixeira^C <tammydbtx@gmail.com>
2024-07-05 20:26:17 -0300

    Adicionei exercicio s05

--- exercicios/para-casa/sistema_de_alunas2.py
+++ exercicios/para-casa/sistema_de_alunas2.py
@@ -8,7 +8,7 @@ def main():
         cod_opcao = obter_opcao()
         
         if cod_opcao == 1: incluir_nova_aluna()
-        elif cod_opcao == 2: consultar_lista_alunas()
+        elif cod_opcao == 2: consultar_lista_aluna()
         elif cod_opcao == 3: consultar_faltas_aluna()
         elif cod_opcao == 4: consultar_notas_aluna()
         elif cod_opcao == 5: consultar_status_aprovacao()
@@ -36,24 +36,62 @@ def obter_opcao():
         return codigo_opcao
     
 def incluir_nova_aluna():
+    try:
+        aluna = input("Insira o nome da aluna nova:")
+        incluir_nova_aluna.append(aluna) 
+    except NameError:
+        print("Entrada inválida. Por favor, insira o nome da aluna nova.")
+
     pass
-    #TODO - Implentar a função
     
-def consultar_lista_alunas():
-    pass
-    #TODO - Implentar a função
     
 def consultar_faltas_aluna():
+    nome = input("Nome da aluna:")
+    sobrenome = input("Sobre nome da aluna:")
+
+    dados = f"{nome} {sobrenome}"
+    if dados in dataset:
+        faltas = dataset[dados]["Presença"].count(False)
+        print(f"A aluna {nome} {sobrenome} possui {faltas} faltas.")
+    else:
+        print("Opção invalida. Tente novamente.")
     pass
-    #TODO - Implentar a função
     
 def consultar_notas_aluna():
+    nome = input("Nome da aluna:")
+    sobrenome = input("Sobrenome da aluna:")
+
+    dados = f"{nome} {sobrenome}"
+    if dados in dataset:
+        notas = dataset[dados]["Notas"]
+        print(f"As notas da aluna {nome} {sobrenome} são:{notas}")
+    else:
+        print("Aluna não encontrada.")
     pass
-    #TODO - Implentar a função
+   
+def consultar_lista_aluna():
+    nome = input("Nome da aluna:")
+    sobrenome = input("Sobrenome da aluna:")
+
+    dados = f"{nome} {sobrenome}"
+    if dados in dataset:
+        consultar_lista_aluna = dataset[dados]["Lista"]
+        print(f"A lista de alunas {nome} {sobrenome} é:{consultar_lista_aluna}")
+    else:
+        print("Aluna não encontrada.")
+    pass
     
 def consultar_status_aprovacao():
-    pass
-    #TODO - Implentar a função
-    
+    nome = input("Nome da aluna:")
+    sobrenome = input("Sobrenome da aluna:")
 
-main()
+    qtd_faltas = dataset[(nome, sobrenome)]["Presença"].count(False) 
+    if qtd_faltas > 2: 
+        return "Reprovada"
+    elif qtd_faltas < 6:
+        return "Reprovada"
+    else:
+        return "Aprovada"
+    pass
+   
+    
