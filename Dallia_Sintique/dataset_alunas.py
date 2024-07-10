d470221db2d741d6819eba991b5b76b820c7f1c5
DalliaSintique <dallia_sintique@outlook.com>
2024-07-10 20:29:11 -0300

    Adicionado pasta Dallia_Sintique

---
+++ Dallia_Sintique/dataset_alunas.py
@@ -0,0 +1,62 @@
+dataset = {
+    ("Ana", "Silva"): {
+        "Turma": "Turma A",
+        "Notas": [7.5, 4.0, 6.0],
+        "Presença": [True, True, False, True, True],
+        "Participação": 8.5
+    },
+    ("Mariana", "Santos"): {
+        "Turma": "Turma B",
+        "Notas": [6.0, 7.5, 8.5],
+        "Presença": [True, True, True, False, True],
+        "Participação": 7.2
+    },
+    ("Carla", "Oliveira"): {
+        "Turma": "Turma A",
+        "Notas": [8.0, 7.5, 8.5],
+        "Presença": [True, True, True, True, True],
+        "Participação": 8.2
+    },
+    ("Juliana", "Ferreira"): {
+        "Turma": "Turma C",
+        "Notas": [9.0, 8.5, 7.0],
+        "Presença": [True, True, True, True, True],
+        "Participação": 8.7
+    },
+    ("Patrícia", "Souza"): {
+        "Turma": "Turma B",
+        "Notas": [7.0, 7.0, 7.5],
+        "Presença": [True, False, True, True, True],
+        "Participação": 7.2
+    },
+    ("Aline", "Martins"): {
+        "Turma": "Turma A",
+        "Notas": [8.5, 8.0, 9.0],
+        "Presença": [True, True, True, True, True],
+        "Participação": 8.5
+    },
+    ("Fernanda", "Costa"): {
+        "Turma": "Turma C",
+        "Notas": [6.5, 7.0, 8.0],
+        "Presença": [True, True, True, False, True],
+        "Participação": 5.5
+    },
+    ("Camila", "Pereira"): {
+        "Turma": "Turma B",
+        "Notas": [7.5, 8.0, 8.5],
+        "Presença": [True, True, True, True, True],
+        "Participação": 8.0
+    },
+    ("Luana", "Rodrigues"): {
+        "Turma": "Turma A",
+        "Notas": [9.0, 9.0, 8.5],
+        "Presença": [True, True, True, True, True],
+        "Participação": 8.8
+    },
+    ("Beatriz", "Lima"): {
+        "Turma": "Turma C",
+        "Notas": [8.0, 7.5, 7.0],
+        "Presença": [True, True, False, False, False],
+        "Participação": 7.5
+    }
+}
